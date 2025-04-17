00b988c2589b57efe76d155bbfdbce3cb52f2883
Érick da silva <eierickdasilva@gmail.com>
2025-04-17 18:43:44 -0300

    feat: adding Kiam sound when tapping on title

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -18,6 +18,7 @@ import { SettingsComponent } from 'src/app/pages/settings/settings.component';
 export class HeaderComponent {
   public title = input.required<string>();
   public load = input(false);
+  private currentAudio: HTMLAudioElement | null = null;
 
   constructor(
     private modalCtrl: ModalController,
@@ -27,5 +28,12 @@ export class HeaderComponent {
     this.modalCtrl.create({
       component: SettingsComponent,
     }).then((m) => m.present());
+  }
+
+  public playKiam() {
+    if (!this.currentAudio || this.currentAudio.paused) {
+      this.currentAudio = new Audio("assets/kiam.mp3");
+      this.currentAudio.play();
+    }
   }
 }
